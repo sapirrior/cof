584e361ea97c3d6b703a89530244615b34ea5efd
Nolan Stark <jimmynnit1000@gmail.com>
2026-03-24 20:44:06 +0530

    remove pre-made builds

--- build/src/utils/file.d
+++
@@ -1,4 +0,0 @@
-/data/data/com.termux/files/home/works/cof/build/src/utils/file.o: \
-  /data/data/com.termux/files/home/works/cof/src/utils/file.c \
-  /data/data/com.termux/files/home/works/cof/src/utils/file.h
-/data/data/com.termux/files/home/works/cof/src/utils/file.h:
